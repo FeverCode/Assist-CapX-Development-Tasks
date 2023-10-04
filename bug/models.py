07b287be1fbf9085c6acdb446accdda5121eca28
FeverCode <giddylancs.gl@gmail.com>
2023-10-04 22:57:48 +0300

    Second task for T34664: created models for bug app and added a bug via Admin panel.

--- bug/models.py
+++ bug/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Bug(models.Model):
+    DESCRIPTION_CHOICES = [
+        ('error', 'Error'),
+        ('feature', 'New Feature'),
+    ]
+    STATUS_CHOICES = [
+        ('todo', 'To Do'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+    ]
+    description = models.TextField()
+    bug_type = models.CharField(
+        max_length=20,
+        choices=DESCRIPTION_CHOICES,
+        default='error'
+    )
+    report_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='todo'
+    )
+    def __str__(self):
+        return f"{self.description} - reported on {self.report_date.strftime('%Y-%m-%d')}"
--- core/settings.py
+++ core/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+    'bug.apps.BugConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bug.apps.BugConfig',
+    
 ]
 
 MIDDLEWARE = [
