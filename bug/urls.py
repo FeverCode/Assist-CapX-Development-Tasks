8c3cc0cfaac65cabc1aa5f6477f9fb855f98efb2
FeverCode <giddylancs.gl@gmail.com>
2023-10-05 14:38:35 +0300

    Update views to use generic Django class-based views

--- bug/urls.py
+++ bug/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import index, view_bug, bug_list
-from . import views
+from .views import BugCreateView, BugDetailView, BugListView
 
 urlpatterns = [
-    path('', index, name='home'),
-    # path('register/', views.register_bug, name='register_bug'),
-    path('bug/<int:bug_id>/', views.view_bug, name='view_bug'),
-    path('bugs/', views.bug_list, name='bug_list'),
+    path('', BugCreateView.as_view(), name='home'),
+    path('bug/<int:bug_id>/', BugDetailView.as_view(), name='view_bug'),
+    path('bugs/', BugListView.as_view(), name='bug_list'),
 ]
--- bug/views.py
+++ bug/views.py
@@ -1,22 +1,31 @@
-from django.shortcuts import render, redirect
+from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BugForm
 from .models import Bug
 
-# Create your views here.
-def index(request):
-    if request.method == 'POST':
+class BugCreateView(View):
+    template_name = 'register_bug.html'
+
+    def get(self, request):
+        form = BugForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
         form = BugForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('bug_list')
-    else:
-        form = BugForm()
-    return render(request, 'register_bug.html', {'form': form})
+        return render(request, self.template_name, {'form': form})
+class BugDetailView(View):
+    template_name = 'view_bug.html'
 
-def view_bug(request, bug_id):
-    bug = Bug.objects.get(pk=bug_id)
-    return render(request, 'view_bug.html', {'bug': bug})
+    def get(self, request, bug_id):
+        bug = get_object_or_404(Bug, pk=bug_id)
+        return render(request, self.template_name, {'bug': bug})
 
-def bug_list(request):
-    bugs = Bug.objects.all()
-    return render(request, 'bug_list.html', {'bugs': bugs})
+class BugListView(View):
+    template_name = 'bug_list.html'
+
+    def get(self, request):
+        bugs = Bug.objects.all()
+        return render(request, self.template_name, {'bugs': bugs})
